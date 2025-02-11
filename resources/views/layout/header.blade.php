de6cbe4192a80980ee5933650cfe3122606ec725
Rakhyan <rakhyanabimanyu@gmail.com>
2025-02-11 21:47:32 +0700

    first commit

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,36 +1,14 @@
 @php
     $currentUser = auth()->user()->load('role');
 @endphp
-<div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+<!-- <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
         data-sidebar-position="fixed" data-header-position="fixed">
         <aside class="left-sidebar">
             <div>
-                <div class="brand-logo d-flex align-items-center justify-content-between">
-                    <a href="{{ url('/') }}" class="text-nowrap logo-img">
-                        <img src="{{ asset('assets/images/logos/logo.png') }}" width="180" alt="" />
-                    </a>
-                    <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
-                        <i class="ti ti-x fs-8"></i>
-                    </div>
-                </div>
+                
                 <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
                     <ul id="sidebarnav">
-                        <li class="sidebar-item mt-3">
-                            <a class="sidebar-link" href="{{ url('/dashboard') }}" aria-expanded="false">
-                                <span>
-                                    <i class="ti ti-layout-dashboard"></i>
-                                </span>
-                                <span class="hide-menu">Dashboard</span>
-                            </a>
-                        </li>
-                        <li class="sidebar-item">
-                            <a class="sidebar-link" href="{{ url('/kuisioner')}}" aria-expanded="false">
-                                <span>
-                                    <i class="ti ti-list"></i>
-                                </span>
-                                <span class="hide-menu">Kuisioner Laporan</span>
-                            </a>
-                        </li>
+                        
                         {{-- <li class="sidebar-item">
                             <a class="sidebar-link" href="" aria-expanded="false">
                                 <span>
@@ -108,4 +86,108 @@
         </div>
         </nav>
       </header>
-</div>
+</div> -->
+
+<div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+    data-sidebar-position="fixed" data-header-position="fixed">
+    <!-- Sidebar Start -->
+    <aside class="left-sidebar">
+      <!-- Sidebar scroll-->
+      <div>
+      <div class="brand-logo d-flex align-items-center justify-content-between">
+            <a href="{{ url('/') }}" class="text-nowrap logo-img">
+                <img src="{{ asset('assets/images/logos/logo.png') }}" width="180" alt="" />
+            </a>
+            <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+                <i class="ti ti-x fs-8"></i>
+            </div>
+        </div>
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+          <ul id="sidebarnav">
+          <li class="sidebar-item mt-3">
+                            <a class="sidebar-link" href="{{ url('/dashboard') }}" aria-expanded="false">
+                                <span>
+                                    <i class="ti ti-layout-dashboard"></i>
+                                </span>
+                                <span class="hide-menu">Dashboard</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="{{ url('/kuisioner')}}" aria-expanded="false">
+                                <span>
+                                    <i class="ti ti-list"></i>
+                                </span>
+                                <span class="hide-menu">Kuisioner Laporan</span>
+                            </a>
+                        </li>
+          </ul>
+        </nav>
+        <!-- End Sidebar navigation -->
+      </div>
+      <!-- End Sidebar scroll-->
+    </aside>
+    <!--  Sidebar End -->
+    <!--  Main wrapper -->
+    <div class="body-wrapper">
+      <!--  Header Start -->
+      <header class="app-header">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <ul class="navbar-nav">
+            <li class="nav-item d-block d-xl-none">
+              <a class="nav-link sidebartoggler nav-icon-hover" id="headerCollapse" href="javascript:void(0)">
+                <i class="ti ti-menu-2"></i>
+              </a>
+            </li>
+          </ul>
+          <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
+            <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
+              <li class="nav-item dropdown">
+                <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  <img src="../assets/images/profile/user-1.jpg" alt="" width="35" height="35" class="rounded-circle">
+                </a>
+                <div class="dropdown-menu content-dd dropdown-menu-end dropdown-menu-animate-up"
+                            aria-labelledby="drop1">
+                            <div class="profile-dropdown position-relative" data-simplebar>
+                                <div class="py-3 px-7 pb-0">
+                                    <h5 class="mb-0 fs-5 fw-semibold">Profile</h5>
+                                </div>
+                                @auth
+                                    <div class="text-center py-9 mx-7 border-bottom">
+                                      <img src="{{ asset('assets/images/profile/user-1.jpg') }}" class="rounded-circle d-block mx-auto"
+                                          width="80" height="80" alt="" />
+                                      <h5 class="mt-3 mb-1 fs-3">{{ $currentUser->name }}</h5>
+                                      <span class="mb-1 d-block text-dark">{{ $currentUser->role->name }}</span>
+                                    </div>  
+                                @endauth
+                                <div class="d-grid py-4 px-7 pt-8">
+                                    <form action="{{ route('logout') }}" method="post" custom-action>
+                                        @csrf
+                                        <button type="submit" class="w-100 btn btn-outline-primary">Log
+                                            Out</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <!--  Header End -->
+      <div class="container-fluid">
+        <!--  Row 1 -->
+        <div class="row">
+          @yield('konten')
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
+  <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/js/sidebarmenu.js"></script>
+  <script src="../assets/js/app.min.js"></script>
+  <script src="../assets/libs/apexcharts/dist/apexcharts.min.js"></script>
+  <script src="../assets/libs/simplebar/dist/simplebar.js"></script>
+  <script src="../assets/js/dashboard.js"></script>
